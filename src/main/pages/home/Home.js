0ce068a37f355071929ab591086111c2f146c7cd
orr1nuycruz <orr1n@hotmail.com>
2023-09-08 01:33:52 -0400

    added content to the home page

--- src/main/pages/home/Home.js
+++ src/main/pages/home/Home.js
@@ -3,8 +3,6 @@ import React from "react";
 const Home = () => {
   return (
     <div className="home-container">
-      <h1 className="home-title">Welcome to our shop</h1>
-
       <div className="home-image-content">
         <div className="home-image-item">
           <div className="image-wrapper">
@@ -50,15 +48,25 @@ const Home = () => {
         </div>
       </div>
 
-       <div className="home-thankyoucard">
-          <div className="home-thankyoucard-image"></div>
-          <div className="home-thankyoucard-title">
+      <div className="home-thankyoucard">
+        <div className="home-thankyoucard-image"></div>
+        <div className="home-thankyoucard-title">
+          <h2 className="home-title">Welcome to our shop</h2>
           <h2>Each package will include the following:</h2>
-            <h2>Custom Product(s) |</h2>
-            <h2>Special Stickers | Freebies |</h2>
-            <h2>Personalized ThankYou Card</h2>
-          </div>
+          <h2>Custom Product(s) |</h2>
+          <h2>Special Stickers | Freebies |</h2>
+          <h2>Personalized ThankYou Card</h2>
         </div>
+      </div>
+
+      <h2 className="deals-title">Special Deals</h2>
+      <div className="deals-content">
+        <div className="deals-content-text">
+          <h2>Buy 2 of any hoodie or crewneck</h2>
+          <h3>Get 1 FREE Tote Bag</h3>
+        </div>
+        <div className="deals-image"></div>
+      </div>
     </div>
   );
 };
