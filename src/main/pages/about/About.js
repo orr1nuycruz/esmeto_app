80639a3f33dc94e0811e12d1aba3fe64573b2c0c
orr1nuycruz <orr1n@hotmail.com>
2023-09-12 00:23:28 -0400

    added content to about page

--- src/main/pages/about/About.js
+++ src/main/pages/about/About.js
@@ -1,15 +1,36 @@
-import React from "react"
+import React from "react";
 
+const About = () => {
+  return (
+    <div className="about-container">
+      <h1 className="about-terms-title">Terms & Conditions</h1>
 
-const About= () =>{
-    return(
-        <div>
-            <h1>
-                About page
-            </h1>
-            
-        </div>
-    )
-}
+      <h2 className="about-terms-subtitle">| Buy 2 Get 1 FREE Tote Bag |</h2>
+      <ul className="about-terms-listitem">
+        <li>will automatically be included in your order if requirements met.</li>
+        <li>MUST PURCHASE 1 hoodie and 1 crewneck. (t-shirts, long sleeve and tote bags excluded).</li>
+      </ul>
 
-export default About
+      <h2 className="about-terms-subtitle">| Order Process |﻿</h2>
+      <ul className="about-terms-listitem">
+        <li>It may take 2-7 business days to process order(s) which includes digitizing your preferred design(s) & embroidering the design onto the product(s).</li>
+      </ul>
+
+      <h2 className="about-terms-subtitle">| Refund Policy |﻿</h2>
+      <ul className="about-terms-listitem">
+        <li>NO refunds will be allowed.</li>
+        <li>Exchanges will be allowed for approved product(s) ONLY</li>
+      </ul>
+
+      <h2 className="about-terms-subtitle">| Cancellation Policy |</h2>
+      <ul className="about-terms-listitem">
+        <li>Customers may only cancel orders 24hrs upon ordering.</li>
+      </ul>
+
+      <h4 className="about-terms-email">Please email esmeTO.11.28@gmail.com or dm @_esmeto_  on IG</h4>
+
+    </div>
+  );
+};
+
+export default About;
